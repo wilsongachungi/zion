e7389267829960a2d3317356409d01bd331583bf
wilsongachungi <gachungiwilson91@gmail.com>
2023-10-15 19:51:21 +0300

    fix creating appointment

--- zionvalley/app/Http/Controllers/HomeController.php
+++ zionvalley/app/Http/Controllers/HomeController.php
@@ -5,36 +5,61 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Appointment;
 
 class HomeController extends Controller
 {
     public function redirect()
     {
 
-        if(Auth::id())
-        {
-            if(Auth::user()->usertype=='0')
-            {
+        if (Auth::id()) {
+            if (Auth::user()->usertype == '0') {
                 return view('user.home');
-            }
-            elseif(Auth::user()->usertype=='2')
-            {
+            } elseif (Auth::user()->usertype == '2') {
                 return view('admin.member');
-            }
-
-            else
-            {
+            } else {
                 return view('admin.home');
             }
-        }
-        else
-        {
+        } else {
             return redirect()->back();
         }
-
     }
 
-    public function index(){
+    public function index()
+    {
         return view('user.home');
+    }
+
+    public function appointment(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'date' => 'required|after:yesterday',
+            'bookfor' => 'required',
+            'phone' => 'required',
+            'message' => 'required',
+        ]);
+
+        // $data = new appointment();
+
+        // $data->name = $request->name;
+        // $data->email = $request->email;
+        // $data->date = $request->date;
+        // $data->phone = $request->phone;
+        // $data->bookfor = $request->bookfor;
+        // $data->message = $request->message;
+
+        $request['status'] = 'In progress';
+
+        if (Auth::id()) {
+            $request['user_id'] = Auth::user()->id;
+        }
+
+        $appointment = Appointment::create($request->all());
+
+        // $data->save();
+
+        return redirect()->back()->with('message', 'Appointment made Successful. Will contact you soon ');
     }
 }
--- zionvalley/resources/views/user/appointment.blade.php
+++ zionvalley/resources/views/user/appointment.blade.php
@@ -2,31 +2,49 @@
     <div class="container">
       <h1 class="text-center wow fadeInUp">Make an Appointment</h1>
 
-      <form class="main-form">
+      <form class="main-form" method="post" action="{{ route('appointment') }}" enctype="multipart/form-data">
+        @csrf
         <div class="row mt-5 ">
-          <div class="col-12 col-sm-6 py-2 wow fadeInLeft">
-            <input type="text" class="form-control" placeholder="Full name">
+          <div class="col-12 col-sm-6 pdy-2 wow fadeInLeft">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Full name" value="{{ old('name') }}">
+            @error('name')
+                <p class="alert alert-danger py-0">{{ $message }}</p>
+            @enderror
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInRight">
-            <input type="text" class="form-control" placeholder="Email address..">
+            <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Email address.." value="{{ old('email') }}">
+            @error('email')
+                <p class="alert alert-danger">{{ $message }}</p>
+            @enderror
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
-            <input type="date" class="form-control">
+            <input type="date" name="date" class="form-control" value="{{ old('date') }}">
+            @error('date')
+                <p class="alert alert-danger">{{ $message }}</p>
+            @enderror
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInRight" data-wow-delay="300ms">
-            <select name="departement" id="departement" class="custom-select">
-              <option value="general">General Health</option>
-              <option value="cardiology">Cardiology</option>
-              <option value="dental">Dental</option>
-              <option value="neurology">Neurology</option>
-              <option value="orthopaedics">Orthopaedics</option>
+            <select name="bookfor" id="bookfor" class="custom-select">
+                <option {{ old('bookfor') === '' ? 'selected' : '' }} value="">Select a service</option>
+                <option {{ old('bookfor') === 'general' ? 'selected' : '' }} value="general">Cottages</option>
+                <option {{ old('bookfor') === 'ziplining' ? 'selected' : '' }} value="ziplining">zip lining</option>
+                <option {{ old('bookfor') === 'nature_walk' ? 'selected' : '' }} value="nature_walk">Nature walk</option>
             </select>
+            @error('bookfor')
+                <p class="alert alert-danger">{{ $message }}</p>
+            @enderror
           </div>
           <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
-            <input type="text" class="form-control" placeholder="Number..">
+            <input type="text" name="phone" class="form-control" placeholder="Number.." value="{{ old('phone') }}">
+            @error('phone')
+                <p class="alert alert-danger">{{ $message }}</p>
+            @enderror
           </div>
           <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
-            <textarea name="message" id="message" class="form-control" rows="6" placeholder="Enter message.."></textarea>
+            <textarea name="message" id="message" class="form-control" rows="6" placeholder="Enter message..">{{ old('message') }}</textarea>
+            @error('message')
+                <p class="alert alert-danger py-0">{{ $message }}</p>
+            @enderror
           </div>
         </div>
 
